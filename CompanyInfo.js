e43097b199df3db830770c4cca66246093c9d13a
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2022-11-14 16:45:43 +0200

    fetching company profile

--- CompanyInfo.js
+++ CompanyInfo.js
@@ -1,1 +1,10 @@
-let symbol = new URLSearchParams(window.location.search).get('symbol');
+let sym = new URLSearchParams(window.location.search).get("symbol");
+
+async function getData(symbol) {
+  let response = await fetch(
+    `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${symbol}`
+  );
+  console.log(response);
+}
+
+getData(sym);
