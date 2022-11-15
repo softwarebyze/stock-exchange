fdd8f16d4e1258fd21d59dfd54b4ad622346cc1e
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2022-11-15 06:33:12 +0200

    presenting company info

--- CompanyInfo.js
+++ CompanyInfo.js
@@ -1,10 +1,23 @@
-let sym = new URLSearchParams(window.location.search).get("symbol");
+const img = document.querySelector("img");
+const companyName = document.querySelector("a");
+const companyDescription = document.querySelector("p");
+
+const sym = new URLSearchParams(window.location.search).get("symbol");
 
 async function getData(symbol) {
-  let response = await fetch(
+  const response = await fetch(
     `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${symbol}`
   );
-  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  showData(data);
 }
 
 getData(sym);
+
+function showData(data) {
+  img.src = data.profile.image;
+  companyName.innerText = data.profile.companyName;
+  companyName.href = data.profile.website;
+  companyDescription.innerText = data.profile.description;
+}
