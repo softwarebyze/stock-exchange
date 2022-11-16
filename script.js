c1dd6c532322302db9816a1395f36d69ac98e5eb
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2022-11-16 22:54:34 +0200

    it's working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const searchInput = document.querySelector("input");
 const searchBtn = document.querySelector("button");
-const resultListDiv = document.querySelector(".list-group");
+const resultListDiv = document.getElementById("results");
 const spinner = document.querySelector(".spinner-border");
 
 function setQueryInURL(searchQuery) {
@@ -11,96 +11,84 @@ function setQueryInURL(searchQuery) {
   history.pushState(null, "", newRelativePathQuery);
 }
 
-async function getTickers() {
-  const searchQuery = searchInput.value;
+async function getTickers(searchQuery) {
   setQueryInURL(searchQuery);
   const url = `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${searchQuery}&limit=10&exchange=NASDAQ`;
+  let tickers = [];
   try {
     const response = await fetch(url);
     const tickerSearchData = await response.json();
-    const tickers = [];
     tickerSearchData.forEach((result) => tickers.push(result.symbol));
-    console.log(tickers);
-    return tickerSearchData;
+    return tickers;
   } catch (err) {
     console.error(err);
   }
 }
 
-async function getCompanyData(symbol) {
-  const response = await fetch(
-    `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${symbol}`
-  );
-  const data = await response.json();
-  return data;
-}
-
-async function showData(searchData) {
-  // Using the number of results, company names, and symbols from the Ticker Search "(searchData)".
-  // Using the logo and the percent change from the Company Search
-
-  resultListDiv.innerHTML = "";
-  // console.log(searchData.length)
-  for (let i = 0; i < searchData.length; i++) {
-    const a = document.createElement("a");
-    // a.href = `./company.html?symbol=${searchData[i].symbol}`; //included
-    // a.classList.add("list-group-item", "list-group-item-action"); //included
-
-    // let aCopiedFromDevTools = `
-    // <a href="./company.html?symbol=AAPL" class="list-group-item list-group-item-action">
-    //   <img src="https://financialmodelingprep.com/images-New-jpg/AAPL.jpg" height="50">
-    //   <span>Apple Inc.</span>
-    //   <span>(AAPL)</span>
-    //   <span style="color: red;">(-1.54%)</span>
-    // </a>
-    // `;
-
-    const companyData = await getCompanyData(searchData[i].symbol);
-
-    // const logoImg = document.createElement("img"); //included
-    // logoImg.src = companyData.profile.image; //included
-    // logoImg.height = "50"; //included
-    // a.appendChild(logoImg); //included
-    // const companyNameSpan = document.createElement("span"); //included
-    // companyNameSpan.innerText = `${searchData[i].name}`; //included
-    // a.appendChild(companyNameSpan); //included
-    // const tickerSpan = document.createElement("span"); //included
-    // tickerSpan.innerText = `(${searchData[i].symbol})`; //included
-    // a.appendChild(tickerSpan); //included
-    // const pctSpan = document.createElement("span"); //included
-
-    const pct = companyData.profile.changesPercentage;
-    let pctColor = pct > 0 ? "green" : "red";
-    let plus = pct > 0 ? "+" : "";
-    // pctSpan.innerText = `(${plus}${parseFloat(pct).toFixed(2)}%)`; //included
-    // pctSpan.style.color = pctColor; //included
-    // a.appendChild(pctSpan); //included
-
-    let aFromDevToolsFilledWithCode = `
-    <a href="./company.html?symbol=${
-      searchData[i].symbol
-    }" class="list-group-item list-group-item-action">
-      <img src="${companyData.profile.image}" height="50">
-      <span>${searchData[i].name}</span>
-      <span>(${searchData[i].symbol})</span>
-      <span style="color: ${pctColor};">(${plus}${parseFloat(pct).toFixed(
-      2
-    )}%)</span>
-    </a>
-    `;
-    a.innerHTML = aFromDevToolsFilledWithCode;
-
-    resultListDiv.appendChild(a);
-    // resultListDiv.appendChild(aFromDevToolsFilledWithCode);
+async function getCompaniesData(tickersArr) {
+  if (tickersArr.length === 1) tickersArr.push(",");
+  const companiesQuery = tickersArr.join();
+  const url = `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${companiesQuery}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const companiesData = await data.companyProfiles;
+    return companiesData;
+  } catch (e) {
+    console.error(e);
   }
 }
 
-async function getAndShow() {
+function getDataForResults(companyProfilesArray) {
+  let resultsData = [];
+  companyProfilesArray.forEach((companyProfile) =>
+    resultsData.push({
+      ticker: companyProfile.symbol,
+      logoUrl: companyProfile.profile.image,
+      companyName: companyProfile.profile.companyName,
+      pctChange: companyProfile.profile.changesPercentage,
+      pctColor: companyProfile.profile.changesPercentage > 0 ? "green" : "red",
+      plus: companyProfile.profile.changesPercentage > 0 ? "+" : "",
+    })
+  );
+  return resultsData;
+}
+
+async function renderResults(containerElement, tickers) {
+  containerElement.innerHTML = "";
+  try {
+    const companyProfiles = await getCompaniesData(tickers);
+    if (!companyProfiles) return;
+    const dataForResults = getDataForResults(companyProfiles);
+    if (containerElement.innerHTML !== "") return;
+
+    dataForResults.forEach((resultData) => {
+      containerElement.innerHTML += `
+      <a href="./company.html?symbol=${resultData.ticker}" class="list-group-item list-group-item-action">
+        <img src="${resultData.logoUrl}" height="50" width="50">
+        <span>${resultData.companyName}</span>
+        <span>(${resultData.ticker})</span>
+        <span style="color: ${resultData.pctColor};">(${resultData.plus}${parseFloat(resultData.pctChange).toFixed(2)}%)</span>
+      </a>
+      `;
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function onSearch() {
+  let inputQuery = searchInput.value;
+  if (!inputQuery) return;
   spinner.classList.remove("d-none");
-  const tickerSearchData = await getTickers();
-  // console.log('tickerSearchData', tickerSearchData);
-  spinner.classList.add("d-none");
-  showData(tickerSearchData);
+  try {
+    const tickers = await getTickers(inputQuery);
+    if (!tickers) return;
+    spinner.classList.add("d-none");
+    renderResults(resultListDiv, tickers);
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 const debounce = (func, wait) => {
@@ -115,10 +103,10 @@ const debounce = (func, wait) => {
   };
 };
 
-searchBtn.addEventListener("click", getAndShow);
-searchInput.addEventListener("input", debounce(getAndShow, 8));
+searchBtn.addEventListener("click", onSearch);
+searchInput.addEventListener("input", debounce(onSearch, 8));
 
 if (window.location.search) {
   searchInput.value = new URLSearchParams(window.location.search).get("query");
-  getAndShow();
+  onSearch();
 }
