0ed0e6cb4003e8df3b14c43591ba5b160740dba2
softwarebyze <77241170+softwarebyze@users.noreply.github.com>
2022-11-20 23:12:02 +0200

    milestone6 done

--- SearchResult.js
+++ SearchResult.js
@@ -1,5 +1,62 @@
 class SearchResult {
-    constructor(results) {
-        this.results = results;
+  constructor(results) {
+    this.results = results;
+  }
+
+  getDataForResults(companyProfilesArray) {
+    let resultsData = [];
+    companyProfilesArray.forEach((companyProfile) =>
+      resultsData.push({
+        ticker: companyProfile.symbol,
+        logoUrl: companyProfile.profile.image,
+        companyName: companyProfile.profile.companyName,
+        pctChange: companyProfile.profile.changesPercentage,
+        pctColor:
+          companyProfile.profile.changesPercentage > 0 ? "green" : "red",
+        plus: companyProfile.profile.changesPercentage > 0 ? "+" : "",
+      })
+    );
+    return resultsData;
+  }
+
+  async getCompaniesData(tickersArr) {
+    if (tickersArr.length === 1) tickersArr.push(",");
+    const companiesQuery = tickersArr.join();
+    const url = `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${companiesQuery}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const companiesData = await data.companyProfiles;
+      return companiesData;
+    } catch (e) {
+      console.error(e);
     }
+  }
+
+  async renderResults(tickers) {
+    let containerElement = this.results;
+    containerElement.innerHTML = "";
+    try {
+      const companyProfiles = await this.getCompaniesData(tickers);
+      if (!companyProfiles) return;
+      const dataForResults = this.getDataForResults(companyProfiles);
+      if (containerElement.innerHTML !== "") return;
+      dataForResults.forEach((resultData) => {
+        containerElement.innerHTML += `
+            <a href="./company.html?symbol=${
+              resultData.ticker
+            }" class="list-group-item list-group-item-action">
+              <img src="${resultData.logoUrl}" height="50" width="50">
+              <span>${resultData.companyName}</span>
+              <span>(${resultData.ticker})</span>
+              <span style="color: ${resultData.pctColor};">(${
+          resultData.plus
+        }${parseFloat(resultData.pctChange).toFixed(2)}%)</span>
+            </a>
+            `;
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
